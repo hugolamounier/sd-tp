40764b1198827940fbe40df6aeca797c91a5f190
Hugo Lamounier <hugolamo@gmail.com>
2022-05-17 19:56:37 -0300

    Create api app; Create Artigo model

---
+++ hugo_sd_tp/api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Artigo
+
+admin.site.register(Artigo)
---
+++ hugo_sd_tp/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Artigo(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+    autor = models.CharField(max_length=80)
+    anoPublicacao = models.CharField(max_length=4)
+    url = models.CharField(max_length=2048)
+
