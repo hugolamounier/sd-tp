8c1a457f3368c4f0d3dc4e231cd44ed5624e061d
Hugo Lamounier <hugolamo@gmail.com>
2022-07-08 18:24:50 -0300

    Add jwt token

--- hugo_sd_tp/hugo_sd_tp/urls.py
+++ hugo_sd_tp/hugo_sd_tp/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', include('api.urls'))
 ]
