ad78c35a884a75c3fd5c4364b42f4f08fe742851
Hugo Lamounier <hugolamo@gmail.com>
2022-07-13 20:41:08 -0300

    Fix roles

--- hugo_sd_tp/hugo_sd_tp/roles.py
+++ hugo_sd_tp/hugo_sd_tp/roles.py
@@ -7,7 +7,13 @@ class Permissions(permissions.BasePermission):
 
     def has_permission(self, request, view):
         user = request.user
-        if request.method in READ_ONLY_METHODS or (user.is_staff and user.is_authenticated):
+        if not user.is_authenticated:
+            return exceptions.NotAuthenticated();
+
+        if user.is_staff:
+            return (user, None)
+
+        if request.method in READ_ONLY_METHODS:
             return (user, None)
         else:
             raise exceptions.PermissionDenied()
